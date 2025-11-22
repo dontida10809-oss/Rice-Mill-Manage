e4272a3074423812ca274e665cb2b8eaf40fc741
dontida10809-oss <dontida10809@gmail.com>
2025-11-22 19:08:38 +0700

    first commit

---
+++ api/config.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+?>
